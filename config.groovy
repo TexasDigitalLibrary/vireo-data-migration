73a496a10abd839a794dd092788485c97183ec0b
scott@scottphillips.com <tomcat@etdpre1.(none)>
2012-09-14 11:09:57 -0500

    Added support for log's with out associated person

--- config.groovy
+++ config.groovy
@@ -1,17 +1,19 @@
 
 
 // Old dspace vireo installation information
-old_db_url = "jdbc:postgresql://hostname:port/database_name"
-old_db_user = "user"
-old_db_pwd = "pass"
-old_asset_path = "/mnt/old_vireo/assetstore/"
+old_db_url = "jdbc:postgresql://etddb1.l/vireo"
+old_db_user = "vireo_old"
+old_db_pwd = "VT1=0adb"
+old_asset_path = "/mnt/vireo_tamu/assetstore/" 
+
 
 // New vireo installation information
-new_db_url = "jdbc:postgresql://hostname:port/database_name"
-new_db_user = "user"
-new_db_pwd = "pass"
-old_asset_path = "/mnt/new_vireo/attachments/"
+new_db_url = "jdbc:postgresql://pgdev3.l/vireo_tamu"
+new_db_user = "vireo_tamu"
+new_db_pwd = "VT1=0abd"
+new_asset_path = "/data/playapps/vireo-data/attachments"
 link_assets = true // Should assets be copied or just symlinked
+
 
 // If you would like an admin account created then turn these settings on.
 create_admin = true
