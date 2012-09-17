b10d4a7cac62ae3ff198a6cea7cf913c835a0530
root <root@ip-10-116-134-69.ec2.internal>
2012-09-17 20:52:45 +0000

    Put in a log line indicating that submissions were imported from existing Vireo

--- log.groovy
+++ log.groovy
@@ -1,79 +1,107 @@
 import groovy.sql.Sql
 
 class log_migrator {
-	static void main(String[] args) {
-		
- 		def config = new ConfigSlurper().parse(new File('config.groovy').toURL())
-		
-		def sql = Sql.newInstance(config.old_db_url,config.old_db_user, config.old_db_pwd)
-		def newsql = Sql.newInstance(config.new_db_url,config.new_db_user, config.new_db_pwd)
-		
-		newsql.execute("delete from actionlog")
-		
-		// Main driving SQL query
+    static void main(String[] args) {
+        
+        def config = new ConfigSlurper().parse(new File('config.groovy').toURL())
+        
+        def sql = Sql.newInstance(config.old_db_url,config.old_db_user, config.old_db_pwd)
+        def newsql = Sql.newInstance(config.new_db_url,config.new_db_user, config.new_db_pwd)
+        
+        newsql.execute("delete from actionlog")
+        
+        // Main driving SQL query
 
-		sql.eachRow("select * from vireolog order by submission_id asc "){ 
-			
-			row -> 
-			
-			// Attachment ID ??
+        sql.eachRow("select * from vireolog order by submission_id asc "){ 
+            
+            row -> 
+            
+            // Attachment ID ??
 
-			if (submissionExists(sql, row.submission_id)) {
+            if (submissionExists(sql, row.submission_id)) {
 
-        def person_id = null;
-        if (personExists(newsql,row.eperson_id))
-          person_id = row.eperson_id;
+            def person_id = null;
+            if (personExists(newsql,row.eperson_id))
+                person_id = row.eperson_id;
 
 
-				def params = [
-				row.log_id, row.date, row.log_entry, (row.private==null?false:row.private), getSubStatus(row.submission_status), null, person_id, row.submission_id
-				]       
-				
-				newsql.execute '''insert into actionlog (
-				id,                     
-				actiondate,                     
-				entry,                  
-				privateflag,                    
-				submissionstate,                        
-				attachment_id,                  
-				person_id,                      
-				submission_id                   
-				)
-				values (
-				?,?,?,?,?,?,?,?
-				)''', params
-			}
-			
-		}
+                def params = [
+                row.log_id, row.date, row.log_entry, (row.private==null?false:row.private), getSubStatus(row.submission_status), null, person_id, row.submission_id
+                ]       
+                
+                newsql.execute '''insert into actionlog (
+                id,                     
+                actiondate,                     
+                entry,                  
+                privateflag,                    
+                submissionstate,                        
+                attachment_id,                  
+                person_id,                      
+                submission_id                   
+                )
+                values (
+                ?,?,?,?,?,?,?,?
+                )''', params
+            }
+            
+        }
 
-                // Update sequence counter
+        // Update sequence counter
 
-                def row = newsql.firstRow("select (max(id) + 1) max from actionlog")
-                newsql.execute("alter sequence seq_actionlog restart with " + row.max)
+        def count = newsql.firstRow("select (max(id) + 1) max from actionlog")
+        newsql.execute("alter sequence seq_actionlog restart with " + count.max)
 
-	}
+	// Now put in a log row for each submission - indicating that the submission was migrated from Vireo 
+        // Note - use of nextval for the id - since we have already updated the sequence number
 
-	// Determine if a submission exists
-	
-	static Boolean submissionExists(Sql sql, Integer subId) {
-		def rows = sql.rows("select submission_id from vireosubmission where submission_id = " + subId)
-		
-		if (rows[0] != null) {
-			return true
-		} else
-		return false
-		
-	}
-	
-	// Return status of a submission
-	
-	static String getSubStatus(Integer stat) {
-		
-		def state = [10:'InProgress', 20:'Submitted', 30:'InReview', 40:'NeedsCorrection', 50:'WaitingOnRequirements', 60:'Approved', 
-		70:'PendingPublication', 80:'Published', 90:'OnHold', 100:'Withdrawn', 110:'Cancelled']
-		
-		return state[stat]
-	}
+        sql.eachRow("select submission_id, status from vireosubmission order by submission_id asc "){
+            row->   
+
+                newsql.execute('''insert into actionlog(
+                                id,
+                                actiondate,                     
+                                entry,                  
+                                privateflag,                    
+                                submissionstate,                        
+                                attachment_id,                  
+                                person_id,                      
+                                submission_id) values (
+                                        nextval('seq_actionlog'),
+                                        localtimestamp,
+                                        ?,
+                                        false, 
+                                        ?, 
+                                        null, 
+                                        null,
+                                        ?)
+                                ''', ["Submission migrated from an existing Vireo system", getSubStatus(row.status), row.submission_id])
+
+        }
+
+
+    }
+
+    // Determine if a submission exists
+    
+    static Boolean submissionExists(Sql sql, Integer subId) {
+        def rows = sql.rows("select submission_id from vireosubmission where submission_id = " + subId)
+        
+        if (rows[0] != null) {
+            return true
+        } else
+        return false
+        
+    }
+    
+    // Return status of a submission
+    
+    static String getSubStatus(Integer stat) {
+        
+        def state = [10:'InProgress', 20:'Submitted', 30:'InReview', 40:'NeedsCorrection', 50:'WaitingOnRequirements', 60:'Approved', 
+        70:'PendingPublication', 80:'Published', 90:'OnHold', 100:'Withdrawn', 110:'Cancelled']
+        
+        return state[stat]
+    }
 
   static boolean personExists(Sql sql, Integer personId) {
     def rows = sql.rows("select id from person where id = "+personId);
